d052a2a646ab113e169bca1bf756a874fe9e313b
Firologame <erwan1207@gmail.com>
2022-08-27 03:42:58 +0200

    auto remove dist files when in dev mode and regenerate them after dev mode finished

--- webpack.config.js
+++ webpack.config.js
@@ -41,7 +41,6 @@ module.exports = {
                     {
                         loader: "css-loader",
                         options: {
-                            modules: true,
                             sourceMap: true
                         }
                     },
